4c3947de20660fb15d566a301c62a92cc953ff90
nakajima1412 <nakajima1412@icloud.com>
2020-03-04 17:07:10 +0900

    refactor (structs v3)

--- app/structs/shapes.go
+++ app/structs/shapes.go
@@ -1,11 +1,17 @@
 package main
 
+// Rectangle has the dimentions of rectangle.
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimerter returns the perimeter of a rectangle.
-func Perimerter(width, height float64) float64 {
-	return 2 * (width + height)
+func Perimerter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area returns the area of a rectangle.
-func Area(width, height float64) float64{
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
