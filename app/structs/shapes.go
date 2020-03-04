36cdc09188bc96fd3ac144001ab7b1a4060e0f95
nakajima1412 <nakajima1412@icloud.com>
2020-03-04 16:52:39 +0900

    add structs v2

--- app/structs/shapes.go
+++ app/structs/shapes.go
@@ -1,5 +1,11 @@
 package main
 
+// Perimerter returns the perimeter of a rectangle.
 func Perimerter(width, height float64) float64 {
 	return 2 * (width + height)
+}
+
+// Area returns the area of a rectangle.
+func Area(width, height float64) float64{
+	return width * height
 }
